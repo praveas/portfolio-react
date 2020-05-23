f4399773b21240720db6a993d8d477cdab56c5c4
Prabesh Pokharel <praveas@gmail.com>
2020-05-23 12:51:43 +1000

    Updates

---
+++ precache-manifest.ae28cf4528d64c0602e6f01c94bd3e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403a46f7de1f2605c84ce0961a84da86",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "d6f8fc5f1985df764369",
+    "url": "/portfolio-react/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "e9df1dc4113509ae94a5",
+    "url": "/portfolio-react/static/js/2.9b428226.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.9b428226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f8fc5f1985df764369",
+    "url": "/portfolio-react/static/js/main.ad1c217a.chunk.js"
+  },
+  {
+    "revision": "fb3203ba594dbdde600d",
+    "url": "/portfolio-react/static/js/runtime-main.0f44d30c.js"
+  }
+]);
