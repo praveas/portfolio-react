084a52ac93bea0ab5c4f6dd130b342cdd1a8117f
Prabesh Pokharel <praveas@gmail.com>
2020-05-23 12:51:07 +1000

    deploy my react test application to github

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+  imagebaseurl: "https://praveas.github.io/react-deploy/",
+  name: "Prabesh Pokharel",
+  role: "Frontend Developer and Data Scientist",
+  linkedinId: "prabesh-pokharel-3369a272",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/prabesh-pokharel-3369a272/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/praveas/",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://fb.com/praveas",
+      className: "fa fa-facebook",
+    },
+  ],
+  aboutme:
+    "I am currently looking for a full-time work. I am a self taught Full Stack Web Developer, currently diving deeper into Flutter applications. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Sydney, Australia",
+  website: "https://praveas.github.io",
+  education: [
+    {
+      UniversityName: "Performance Education",
+      specialization: "Professional Year",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2018",
+      Achievements: "Learned about workplace environment",
+    },
+    {
+      UniversityName: "Central Queensland University",
+      specialization: "Masters in Information Systems",
+      MonthOfPassing: "Jul",
+      YearOfPassing: "2017",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
